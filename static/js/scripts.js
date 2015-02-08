f65be397c8dc2015491e786767bc87930e34a81d
Jeff Wang <jeffreywang93@gmail.com>
2015-02-07 22:00:42 -0500

    angular works for now google maps is slow

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,18 +1,84 @@
 // This example displays a marker at the center of Australia.
 // When the user clicks the marker, an info window opens.
 
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
 
 app.controller('MapCtrl', ['$scope', '$http', function($scope, $http) {
   function init() {
+    var myLatlng = new google.maps.LatLng(41.8237487,-71.4002502);
+    var mapOptions = {
+      zoom: 15,
+      center: myLatlng
+    };
+
+    $scope.map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+    $scope.map.markers = [];
+    $scope.poi = [];
+
     $scope.tripAdvisorRequest();
+    google.maps.event.addDomListener(window, 'load', $scope.populateMap);
   }
 
   $scope.tripAdvisorRequest = function () {
-    $http.get('api/landmarks/providence').success(function (data) {
+    $http.get('api/landmarks/providence/').success(function (data) {
       $scope.dataPoints = data;
       console.log($scope.dataPoints);
     });
+  };
+
+  function createMarker(infowindow, point, name) {
+      var marker = new google.maps.Marker({
+          position: point,
+          map: $scope.map,
+          title: name
+      });
+
+      google.maps.event.addListener(marker, 'click', function() {
+        if ($scope.curInfoWindow != undefined) {
+          $scope.curInfoWindow.close();
+        }
+
+        $scope.curInfoWindow = infowindow;
+        $scope.curInfoWindow.open($scope.map, marker);
+      });
+
+      $scope.map.markers.push(marker);
+  }
+
+  $scope.populateMap = function() {
+    var landmarks = $scope.dataPoints.landmarks;
+    for (var i = 0; i < landmarks.length; i++) {
+      var point = new google.maps.LatLng(landmarks[i].latitude, landmarks[i].longitude);
+      $scope.poi.push(point);
+      var contentString = '<div class="marker-popup" id="content">'+
+          '<div id="siteNotice">'+
+          '</div>'+
+          '<h1 id="firstHeading" class="firstHeading">' +
+          landmarks[i].name +
+          '</h1>'+
+          '<div id="bodyContent">'+
+          '<p>' +
+          landmarks[i].description +
+          '</p>'+
+          '</div>'+
+          '</div>';
+
+      var infowindow = new google.maps.InfoWindow({
+          content: contentString
+      });
+
+      createMarker(infowindow, point, landmarks[i].name);
+    }
+
+    var tourPath = new google.maps.Polyline({
+      path: $scope.poi,
+      geodesic: true,
+      strokeColor: '#FF0000',
+      strokeOpacity: 1.0,
+      strokeWeight: 2
+    });
+
+    tourPath.setMap($scope.map);
   };
 
   init();
@@ -106,4 +172,3 @@ function initialize() {
   tourPath.setMap(map);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
